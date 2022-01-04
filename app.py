f38665cf570a4dd71153450a65a01976b8b6c66e
Daniel Wong <suchunw@hotamil.com>
2022-01-04 16:06:51 +0800

    Change connection string

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import pymongo
 from flask import Flask, jsonify
 
 load_dotenv() # use dotenv to hide sensitive credential as environment variables
-DATABASE_URL=f'mongodb+srv://user:{os.environ.get("password")}'\
-			  '@mongo-heroku-cluster-we.kyrhz.mongodb.net/myFirstDatabase?'\
-			  'retryWrites=true&w=majority' # get connection url from environment
+# DATABASE_URL=f'mongodb+srv://user:{os.environ.get("password")}'\
+# 			  '@mongo-heroku-cluster-we.kyrhz.mongodb.net/myFirstDatabase?'\
+# 			  'retryWrites=true&w=majority' # get connection url from environment
+DATABASE_URL=f'mongodb+srv://danwsc:{os.environ.get("password")})@cluster0.x2ocq.mongodb.net/test?retryWrites=true&w=majority'
 
 client=pymongo.MongoClient(DATABASE_URL) # establish connection with database
 mongo_db=client.db # assign database to mongo_db
