6bb4a485e5be7229dac9590df9b29bd89234bd81
Daniel Wong <suchunw@hotamil.com>
2022-01-05 19:40:43 +0800

    Change db to test

--- app_basic.py
+++ app_basic.py
@@ -7,10 +7,10 @@ load_dotenv() # use dotenv to hide sensitive credential as environment variables
 # DATABASE_URL=f'mongodb+srv://user:{os.environ.get("password")}'\
 # 			  '@mongo-heroku-cluster-we.kyrhz.mongodb.net/myFirstDatabase?'\
 # 			  'retryWrites=true&w=majority' # get connection url from environment
-DATABASE_URL=f'mongodb+srv://danwsc:{os.environ.get("password")})@cluster0.x2ocq.mongodb.net/test?retryWrites=true&w=majority'
+DATABASE_URL=f'mongodb+srv://danwsc:{os.environ.get("password")}@cluster0.x2ocq.mongodb.net/test?retryWrites=true&w=majority'
 
 client=pymongo.MongoClient(DATABASE_URL) # establish connection with database
-mongo_db=client.db # assign database to mongo_db
+mongo_db=client.test # assign database to mongo_db
 
 app=Flask(__name__)
 
--- load_data.py
+++ load_data.py
@@ -4,12 +4,13 @@ import os
 import pymongo
 
 load_dotenv() # use dotenv to hide sensitive credential as environment variables
-DATABASE_URL=f'mongodb+srv://user:{os.environ.get("password")}'\
-			  '@mongo-heroku-cluster-we.kyrhz.mongodb.net/myFirstDatabase?'\
-			  'retryWrites=true&w=majority' # get connection url from environment
+# DATABASE_URL=f'mongodb+srv://user:{os.envirosn.get("password")}'\
+# 			  '@mongo-heroku-cluster-we.kyrhz.mongodb.net/myFirstDatabase?'\
+# 			  'retryWrites=true&w=majority' # get connection url from environment
+DATABASE_URL=f'mongodb+srv://danwsc:{os.environ.get("password")}@cluster0.x2ocq.mongodb.net/test?retryWrites=true&w=majority'
 
 client=pymongo.MongoClient(DATABASE_URL) # establish connection with database
-mongo_db=client.db # assign database to mongo_db
+mongo_db=client.test # assign database to mongo_db
 mongo_db.launches.drop() # clear the collection
 
 with open('static/data/launches.json') as file: # opening the json file
